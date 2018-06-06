45590968e70b65a643879fd728804bcf3edd67e0
sprea <spriha27@gmail.com>
2018-06-06 17:23:01 +0530

    Added checkWinner function and small changes

--- Scarne/app/src/main/java/com/example/android/scarne/MainActivity.java
+++ Scarne/app/src/main/java/com/example/android/scarne/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     int[] drawables ={R.drawable.dice1,R.drawable.dice2,R.drawable.dice3,R.drawable.dice4,R.drawable.dice5,R.drawable.dice6};
 
     TextView text;
+    TextView winnerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +44,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void rollButtonClick(View v){
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                winnerView.setText("");
+//            }
+//        });
         int number = rollDice();
         ImageView imageView = findViewById(R.id.dice_image);
         imageView.setImageResource(drawables[number-1]);
@@ -58,21 +65,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void holdButtonClick(View v){
+
         scorePlayer+=turnScorePlayer;
         turnScorePlayer=0;
 
         Text = userScoreText + scorePlayer + compScoreText + scoreComputer;
         text.setText(Text);
 
+        checkWinner();
+
         computerTurn();
     }
 
-    public void resetButtonClick(View v){
+    public void checkWinner(){
+        if(scoreComputer>=50){
+            winnerView = findViewById(R.id.winner);
+            winnerView.setText("Computer won!");
+            resetButtons();
+        }
+        else if(scorePlayer>=50){
+            winnerView = findViewById(R.id.winner);
+            winnerView.setText("You won!");
+            resetButtons();
+        }
+
+    }
+
+    public void resetButtons(){
         scoreComputer=0;
         scorePlayer=0;
         turnScoreComputer=0;
         turnScorePlayer=0;
+    }
 
+    public void resetButtonClick(View v){
+        resetButtons();
         Text = userScoreText + scorePlayer + compScoreText + scoreComputer;
         text.setText(Text);
     }
@@ -87,78 +114,68 @@ public class MainActivity extends AppCompatActivity {
     public void computerTurn() {
         enableButtons(false);
 
-        while (true) {
-            int number = rollDice();
-            ImageView imageView = findViewById(R.id.dice_image);
-            imageView.setImageResource(drawables[number - 1]);
+        Thread thread = new Thread(){
+            int number;
+            ImageView imageView;
+            @Override
+            public void run(){
+                while (true) {
+                    number = rollDice();
+                    imageView = findViewById(R.id.dice_image);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageResource(drawables[number - 1]);
+                        }
+                    });
 
-            if (number == 1) {
-                turnScoreComputer = 0;
-                Text = userScoreText + scorePlayer + compScoreText + scoreComputer + compTurnScoreText + turnScoreComputer;
-                text.setText(Text);
-                enableButtons(true);
-                return;
-            } else {
-                turnScoreComputer += number;
-                Text = userScoreText + scorePlayer + compScoreText + scoreComputer + compTurnScoreText + turnScoreComputer;
-                text.setText(Text);
+                    if (number == 1) {
+                        turnScoreComputer = 0;
+                        Text = userScoreText + scorePlayer + compScoreText + scoreComputer + compTurnScoreText + turnScoreComputer;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                text.setText(Text);
+                                enableButtons(true);
+                            }
+                        });
+                        return;
+                    }
+                    else {
+                        turnScoreComputer += number;
+                        Text = userScoreText + scorePlayer + compScoreText + scoreComputer + compTurnScoreText + turnScoreComputer;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                text.setText(Text);
+                            }
+                        });
+                    }
+
+                    if (turnScoreComputer > 10) {
+                        scoreComputer += turnScoreComputer;
+                        turnScoreComputer = 0;
+
+                        Text = userScoreText + scorePlayer + compScoreText + scoreComputer;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                text.setText(Text);
+                                enableButtons(true);
+                                checkWinner();
+                            }
+                        });
+                        return;
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
             }
 
-            if (turnScoreComputer > 20) {
-                scoreComputer += turnScoreComputer;
-                turnScoreComputer = 0;
-
-                Text = userScoreText + scorePlayer + compScoreText + scoreComputer;
-                text.setText(Text);
-                enableButtons(true);
-                return;
-            }
-        }
-//        myTimer = new Timer();
-//        myTimer.schedule(new TimerTask() {
-//            @Override
-//            public void run(){
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        enableButtons(false);
-//                    }
-//                });
-//
-//                int number = rollDice();
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        ImageView imageView = findViewById(R.id.dice_image);
-//                        imageView.setImageResource(drawables[number-1]);
-//                    }
-//                });
-//
-//                if(number==1) {
-//                    turnScoreComputer = 0;
-//                    Text = userScoreText + scorePlayer + compScoreText + scoreComputer + compTurnScoreText + turnScoreComputer;
-//
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            enableButtons(true);
-//                        }
-//                    });
-//
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            text.setText(Text);
-//                        }
-//                    });
-//
-//                    myTimer.cancel();
-//                }
-//                else {
-//
-//                }
-//            }
-//        })
-        }
+        };
+        thread.start();
     }
 }
